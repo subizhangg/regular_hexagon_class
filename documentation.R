087f67d109730b5d66e2f5f20594f5d4636fdf66
Qingxue Zhang <qingxue_zhang@outlook.com>
2016-12-18 15:30:56 -0500

    add documentation

---
+++ documentation.R
@@ -0,0 +1,23 @@
+library(devtools)
+
+#' An S3 class to represent a regular hexagon
+#' regular hexagon
+#' @param side A number to represent the length of the side
+#' @return A regular hexagon object with side value
+#' @export
+#' @examples
+#' regular_hexagon(3)
+
+#' area.regular_hexagon() function
+#' @return The area of a regular hexagon with \code{side} 
+#' @examples
+#' area.regular_hexagon(regular_hexagon(3))
+
+
+#' perimeter.regular_hexagon() function
+#' @return The perimeter of a regular hexagon with \code{side}
+#' @examples
+#' perimeter.regular_hexagon(regular_hexagon(3))
+
+
+
